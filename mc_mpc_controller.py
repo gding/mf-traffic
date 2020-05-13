cde24a60ae96ecfe3c3736a9272fcbeda80f83e0
Geoffrey <gding@berkeley.edu>
2020-05-12 22:35:24 -0700

    fixed spacing

--- mc_mpc_controller.py
+++ mc_mpc_controller.py
@@ -19,23 +19,23 @@ def mc_mpc_controller(g,x,horizon=30/0.1,time_step=0.1, num_runs=100):
     	u2 = np.random.random()
     	matrix[0][1] = 1 - u1
     	matrix[0][2] = u1
-	    matrix[1][3] = 1 - u2
-	    matrix[1][4] = u2
-	    return matrix
+	matrix[1][3] = 1 - u2
+	matrix[1][4] = u2
+	return matrix
 
     min_cost = np.inf
     good_u = None
 
     for _ in range(num_runs)
     	# maybe we want simulation_one_player to return (x,u)
-		f_mc = g.simulation_one_player(x, horizon, time_step, random_controller, [6])
+        f_mc = g.simulation_one_player(x, horizon, time_step, random_controller, [6])
     	run_cost = get_cost(f_mc)
 
     	# if we've found a better trajectory...
     	if run_cost < min_cost
-			# find the control input at the first iteratoin
-			good_u = None
-			# TODO. If input is returned as discussed in line 30 above, good_u = u[0,:,:] or something like that 
-    		min_cost = run_cost
+            # find the control input at the first iteratoin
+            good_u = None
+            # TODO. If input is returned as discussed in line 30 above, good_u = u[0,:,:] or something like that 
+            min_cost = run_cost
 
 	return good_u
